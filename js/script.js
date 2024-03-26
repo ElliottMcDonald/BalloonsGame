ff6dbe075e8be69daf458fb767535c19a86a0d4e
ElliottMcDonald <e.mcdonald657@gmail.com>
2024-03-26 21:09:58 +0000

    Add movement to object and collision detection. Add y-axis movement on keydown to shark object. Ensure this triggers once game is started and not earlier. Prevent default scrolling of browser. Add collision detection between shark and fish objects. Remove gameStart on enter and spacebar inputs to reduce complexity and duplication.

--- js/script.js
+++ js/script.js
@@ -4,15 +4,58 @@ const jsStartButton = document.getElementById("js-start-button");
 
 const jsWelcomeMessage = document.getElementById("js-welcome-message");
 
+const jsContainer = document.getElementById("js-container");
+
+const jsShark = document.getElementById("js-shark");
+
+let userReady = false;
+
 let gameStart = () => {
   jsWelcomeMessage.style.visibility = "hidden";
   userReady = true;
 };
 
-jsStartButton.addEventListener("click", () => gameStart());
+jsStartButton.addEventListener("click", () => {
+  gameStart();
+});
 
-window.addEventListener("keydown", function (event) {
-  if (event.key === "Enter" || event.key === " ") {
-    gameStart();
+let moveShark = function (event) {
+  event.preventDefault();
+
+  let step = 15;
+
+  let currentPosition = parseInt(jsShark.style.top) || 0;
+
+  switch (event.key) {
+    case "ArrowUp":
+      currentPosition = Math.max(currentPosition - step, 0);
+      break;
+    case "ArrowDown":
+      currentPosition = Math.min(
+        currentPosition + step,
+        jsContainer.clientHeight - jsShark.clientHeight
+      );
+      break;
+  }
+
+  jsShark.style.top = currentPosition + "px";
+};
+
+window.addEventListener("keydown", (event) => {
+  if (userReady === true) {
+    moveShark(event);
   }
 });
+
+function fishSharkCollision(jsFish, jsShark) {
+  if (
+    jsFish.x + jsFish.width >= jsShark.x &&
+    jsFish.x <= jsShark.x + jsShark.width &&
+    jsFish.y + jsFish.height >= jsShark.y &&
+    jsFish.y <= jsShark.y + jsShark.height
+  )
+    return true;
+  else {
+    return false;
+  }
+}
